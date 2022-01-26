8948325e85ec0adfaad0176037448ec691da5a2e
NFoithong <n.foithong1983@gmail.com>
2022-01-25 22:04:14 -0500

    finish player object

--- __test__/player.test.js
+++ __test__/player.test.js
@@ -75,3 +75,34 @@ test("substracts from player's health", () => {
 
     expect(player.health).toBe(0);
 });
+
+//write the tests and method to get a player's attack value
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+//check that a Potion was added correctly.
+test("adds a potion to the inventory", () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+//write tests that ensure that usePotion() removes the correct Potion from the Player inventory
+test("uses a potion form inventory", () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
